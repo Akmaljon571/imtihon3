77ac8463fbf59f61e388df58cb6c7a4355e03fab
Akmaljon571 <ahmadjonovakmaljon571@yandex.ru>
2022-04-20 14:36:20 +0300

    imtihon

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// // 1-misol
+// var arr = ['I', 'study', 'JavaScript', 'right', 'now'];
+// let deleteArr = arr.splice(2,1);
+// console.log(arr);
+// console.log(deleteArr);
+
+
+// 2-misol
+// let arr = "salom dunyo"
+
+// function salom(arr) {
+//    let A =  arr.split('');
+//    for (const key of A) {
+     
+//     let B = (A.reverse(key));
+//     return B.join('')
+//    }
+// }
+// let jami  = salom(arr)
+// console.log(jami);
+
+// 3-misol
+
+// let kirit = Number(prompt('son kiritng'));
+// function name1(son) {
+//     if (son % 2 == 0){
+//         return true
+//     } else if(son % 2 == 1){
+//        return false
+//     }
+// }
+// let jami = name1(kirit)
+// console.log(jami);
